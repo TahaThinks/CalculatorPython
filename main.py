0dc6fd73aac0fbb9f16a6f85be2e8465a2113d25
TahaThinks <tahaa4587@gmail.com>
2024-01-11 09:34:42 +0400

    Get number of days in a month, even in Leap Year

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-# Functions with outputs
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return("Leap year")
+      else:
+        return("Not leap year")
+    else:
+      return("Leap year")
+  else:
+    return("Not leap year")
+  
+# TODO: Add more code here 👇
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    isLeapYear = is_leap(year)
+    if isLeapYear == "Leap year":
+        month_days[1] = 29
+    return month_days[month-1]
+    
 
-def format_name(fname, lname):
-    formated_fname = fname.title()
-    formated_lname = lname.title()
-    return (f"{formated_fname} {formated_lname}")
 
-formated_name = format_name("TAha", "HussEIN")
-print(formated_name)
+  
+#🚨 Do NOT change any of the code below 
+year = int(input()) # Enter a year
+month = int(input()) # Enter a month
+days = days_in_month(year, month)
+print(days)
