5e97862371008d7b0c7ea2373524dc30201a2e72
TahaThinks <tahaa4587@gmail.com>
2024-01-11 09:14:47 +0400

    Function to Format Name Properly

---
+++ main.py
@@ -0,0 +1,8 @@
+# Functions with outputs
+
+def format_name(fname, lname):
+    formated_fname = fname.title()
+    formated_lname = lname.title()
+    print(f"{formated_fname} {formated_lname}")
+
+format_name("TAha", "HussEIN")
